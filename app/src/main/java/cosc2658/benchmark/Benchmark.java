e2609e7dbb9c5ab87600fa675a4204d13787fcf4
Le Thien Son <lethienson@Les-MacBook-Pro-7.local>
2024-12-19 00:59:24 +0700

    added jupyter notebook file to visualize data

--- app/src/main/java/cosc2658/benchmark/Benchmark.java
+++ app/src/main/java/cosc2658/benchmark/Benchmark.java
@@ -14,7 +14,7 @@ public class Benchmark {
 
   // Benchmarking constants
   private static final int[] GRID_SIZES = {3, 4, 5, 6, 7, 8};
-  private static final int NUM_ITERATIONS = 1;
+  private static final int NUM_ITERATIONS = 20;
 
   public static void main(String[] args) {
     System.out.println("Starting benchmark...");
@@ -29,61 +29,65 @@ public class Benchmark {
       writer.append("GridSize,InitializationTime,MemoryUsage,"
                     + "AverageCalculationTime,IsAllWildcards\n");
 
-      // Iterate through the grid sizes
-      for (int size : GRID_SIZES) {
-        System.out.println("\n#################################################"
-                           + "#########################\n");
-        System.out.println("Grid Size: " + size + "\n");
+      for (int i = 0; i < NUM_ITERATIONS; i++) {
+        // Iterate through the grid sizes
+        for (int size : GRID_SIZES) {
+          System.out.println(
+              "\n#################################################"
+              + "#########################\n");
+          System.out.println("Grid Size: " + size + "\n");
 
-        /*
-         * ALL WILDCARDS BENCHMARKING
-         * */
+          /*
+           * ALL WILDCARDS BENCHMARKING
+           * */
 
-        System.out.println("\tALL WILDCARDS:");
+          System.out.println("\tALL WILDCARDS:");
 
-        String instruction = "*".repeat(size * size);
+          String instruction = "*".repeat(size * size);
 
-        // Benchmark the grid for the current size
-        long initializationTime = benchmarkInitialization(size, instruction);
-        double averageCalculationTime =
-            benchmarkCalculationTime(size, instruction);
-        Runtime runtime = Runtime.getRuntime();
-        long memoryUsage = getMemoryUsage(runtime);
+          // Benchmark the grid for the current size
+          long initializationTime = benchmarkInitialization(size, instruction);
+          double averageCalculationTime =
+              benchmarkCalculationTime(size, instruction);
+          Runtime runtime = Runtime.getRuntime();
+          long memoryUsage = getMemoryUsage(runtime);
 
-        // Write results to the CSV file
-        writer.append(String.format("%d,%d,%d,%d,1\n", size, initializationTime,
-                                    memoryUsage, (long)averageCalculationTime));
-        writer.flush(); // Ensure the data is written immediately
+          // Write results to the CSV file
+          writer.append(String.format("%d,%d,%d,%d,1\n", size,
+                                      initializationTime, memoryUsage,
+                                      (long)averageCalculationTime));
+          writer.flush(); // Ensure the data is written immediately
 
-        System.out.println("\n-------------------------------------------------"
-                           + "-------------------------\n");
-        /*
-         * RANDOM INSTRUCTION BENCHMARKING:
-         *-------------------------------------
-         * For random instructions benchmarking while maintaining the same
-         *affect that the fixed directions have on the grid, the number of fixed
-         *characters are default to N with N being the grid size (N x N)
-         * */
+          System.out.println(
+              "\n-------------------------------------------------"
+              + "-------------------------\n");
+          /*
+           * RANDOM INSTRUCTION BENCHMARKING:
+           *-------------------------------------
+           * For random instructions benchmarking while maintaining the same
+           *affect that the fixed directions have on the grid, the number of
+           *fixed characters are default to N with N being the grid size (N x N)
+           * */
 
-        System.out.println("\tFIXED DIRECTIONS INCLUDED:");
-        instruction = Grid.generateRandomInstruction(size);
-        System.out.println("DIRECTIONS: " + instruction);
+          System.out.println("\tFIXED DIRECTIONS INCLUDED:");
+          instruction = Grid.generateRandomInstruction(size);
+          System.out.println("DIRECTIONS: " + instruction);
 
-        // Benchmark the grid for the current size
-        long fixedInitializationTime =
-            benchmarkInitialization(size, instruction);
-        double fixedAverageCalculationTime =
-            benchmarkCalculationTime(size, instruction);
-        Runtime fixedRuntime = Runtime.getRuntime();
-        long fixedMemoryUsage = getMemoryUsage(runtime);
+          // Benchmark the grid for the current size
+          long fixedInitializationTime =
+              benchmarkInitialization(size, instruction);
+          double fixedAverageCalculationTime =
+              benchmarkCalculationTime(size, instruction);
+          Runtime fixedRuntime = Runtime.getRuntime();
+          long fixedMemoryUsage = getMemoryUsage(runtime);
 
-        // Write results to the CSV file
-        writer.append(String.format("%d,%d,%d,%.2f,0\n", size,
-                                    fixedInitializationTime, fixedMemoryUsage,
-                                    fixedAverageCalculationTime));
-        writer.flush(); // Ensure the data is written immediately
+          // Write results to the CSV file
+          writer.append(String.format("%d,%d,%d,%d,0\n", size,
+                                      fixedInitializationTime, fixedMemoryUsage,
+                                      (long)fixedAverageCalculationTime));
+          writer.flush(); // Ensure the data is written immediately
+        }
       }
-
     } catch (IOException e) {
       e.printStackTrace();
     }
@@ -123,14 +127,13 @@ public class Benchmark {
 
     // Measure calculation time over multiple iterations
     long totalTime = 0;
-    for (int i = 0; i < NUM_ITERATIONS; i++) {
-      long startTime = System.currentTimeMillis();
-      int allPaths = grid.findAllPaths();
-      long endTime = System.currentTimeMillis();
-      System.out.println("Total paths: " + allPaths);
-      totalTime += (endTime - startTime);
-    }
-    double result = (totalTime / (double)NUM_ITERATIONS);
+    long startTime = System.currentTimeMillis();
+    int allPaths = grid.findAllPaths();
+    long endTime = System.currentTimeMillis();
+    System.out.println("Total paths: " + allPaths);
+    totalTime += (endTime - startTime);
+
+    double result = totalTime;
     System.out.println("Calculation Time: " + App.formatNumber((long)result) +
                        " ms");
     return result;
